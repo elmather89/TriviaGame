38ff7e22d44621cf44b6d4d0aecf79a9829be17b
Sansevieria <elizabethleighmather@gmail.com>
2019-03-30 18:58:36 -0500

    save

---
+++ assets/javascript/app2.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // global variables ===============================
+    var q1 = ["What ingredient in milk is eventually devoured by bacteria, causing the sour taste?", "Lactose", "Legumes", "Butter"];
+    var q2 = ["What is the 'groundnut' better known as?", "Acorn", "Cashew", "Peanut"];
+    var q3 = ["What was the first frozen vegetable besides spinach?", "Carrots", "Peas", "Corn"];
+    var q4 = ["What Indian dish is also the national dish of England?", "Saag Paneer", "Baingan Bharta", "Chicken Tikka Masala"]
+    var q5 = ["Who invented Coca-Cola?", "Bernie Sanders", "John Pemberton", "Warren Buffet"];
+
+    var timer = 20;
+    var correctA = 0;
+    var wrongA = 0;
+    var unAnswered = 0;
+
+    // generate a start button
+
+    // generate buttons from a questions array
+
+    // on button click,
+    // remove start button,
+    // load first question,
+    // start the timer countdown (stop it at 0)
+    
+    // 
+
+}) // everything inside this
