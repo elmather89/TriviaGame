340dfacff56aefeb9e742cbf76e49b1c1d019750
Sansevieria <elizabethleighmather@gmail.com>
2019-03-30 20:43:06 -0500

    first questions and answer buttons are loading

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,26 +1,62 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // global variables ===============================
-    var q1 = ["What ingredient in milk is eventually devoured by bacteria, causing the sour taste?", "Lactose", "Legumes", "Butter"];
-    var q2 = ["What is the 'groundnut' better known as?", "Acorn", "Cashew", "Peanut"];
-    var q3 = ["What was the first frozen vegetable besides spinach?", "Carrots", "Peas", "Corn"];
-    var q4 = ["What Indian dish is also the national dish of England?", "Saag Paneer", "Baingan Bharta", "Chicken Tikka Masala"]
-    var q5 = ["Who invented Coca-Cola?", "Bernie Sanders", "John Pemberton", "Warren Buffet"];
+  // global variables ===============================
+  var q1 = ["What ingredient in milk is eventually devoured by bacteria, causing the sour taste?", "Lactose", "Legumes", "Butter"];
+  var q2 = ["What is the 'groundnut' better known as?", "Acorn", "Cashew", "Peanut"];
+  var q3 = ["What was the first frozen vegetable besides spinach?", "Carrots", "Peas", "Corn"];
+  var q4 = ["What Indian dish is also the national dish of England?", "Saag Paneer", "Baingan Bharta", "Chicken Tikka Masala"]
+  var q5 = ["Who invented Coca-Cola?", "Bernie Sanders", "John Pemberton", "Warren Buffet"];
+  var allQuestions = [q1, q2, q3, q4, q5];
 
-    var timer = 20;
-    var correctA = 0;
-    var wrongA = 0;
-    var unAnswered = 0;
+  var timer = 20;
+  var questionTracker = 0;
+  var correctA = 0;
+  var wrongA = 0;
+  var unAnswered = 0;
 
-    // generate a start button
+  startBtn();
+  // generate a start button
+  function startBtn() {
+    var startButton = $("<button>");
+    startButton.addClass("start");
+    startButton.text("START");
+    $("#strt-btn-apnd").append(startButton);
+  };
 
-    // generate buttons from a questions array
-
-    // on button click,
+  // on start button click,
     // remove start button,
     // load first question,
-    // start the timer countdown (stop it at 0)
+    // start the timer countdown
+  $(".start").on("click", function () {
+    $(".start").remove();
+    loadQuestion();
+    startTimer(); // define this function +++++++++
+  });
+
+  function loadQuestion() {
+      $(".main-div").empty();
+
+      // question
+      var qDiv = $("<div>");
+      qDiv.text(allQuestions[0][0]);
+      $(".main-div").append(qDiv);
+
+      // answer buttons
+      for (var i = 1; i < q1.length; i++) {
+          var aDiv = $("<button>");
+          aDiv.text(q1[i]);
+          aDiv.addClass("q-btns");          
+          $(".main-div").append(aDiv);
     
-    // 
+          console.log(allQuestions[0][0]);
+      };
+  };
+
+  // once timer hits 0,
+    // function to check for correctA, wrongA, or unAnswered and tally
+    // go to next question
+  function startTimer() {
+    // ..
+  };
 
 }) // everything inside this
